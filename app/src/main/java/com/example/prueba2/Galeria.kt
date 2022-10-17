2ddae90ceb61c8b3d54171076a7118e269196a9d
Ultron <duartemattias3@gmail.com>
2022-10-17 14:12:34 -0400

    add nav and background

--- app/src/main/java/com/example/prueba2/Galeria.kt
+++ app/src/main/java/com/example/prueba2/Galeria.kt
@@ -3,20 +3,18 @@ package com.example.prueba2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toolbar
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_galeria.*
 
-class MainActivity : AppCompatActivity() {
+class Galeria : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_galeria)
 
         getSupportActionBar()?.hide();
 
-        txtNavLogin.setOnClickListener{
-            val intent = Intent( this,Galeria::class.java )
+        btnRegistro.setOnClickListener{
+            val intent = Intent( this,MainActivity::class.java)
             startActivity(intent)
         }
     }
-
 }
